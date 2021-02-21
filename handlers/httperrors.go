3d607c9b2524f69bc6359c79c92267c22ed5dadf
matscus <matscus@yandex.ru>
2021-02-21 12:16:27 +0300

    func http error

---
+++ handlers/httperrors.go
@@ -0,0 +1,15 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+)
+
+//WriteHTTPError - default write error func
+func WriteHTTPError(w http.ResponseWriter, httpStatusCode int, err error) {
+	w.WriteHeader(httpStatusCode)
+	_, errWrite := w.Write([]byte("{\"Message\":\"" + err.Error() + "\"}"))
+	if errWrite != nil {
+		log.Printf("[ERROR] Not Writing to ResponseWriter error %s due: %s", err.Error(), errWrite.Error())
+	}
+}
